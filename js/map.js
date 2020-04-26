5054368ca8fb3bdd5c74d9557e217c41084ebd44
Betty <liza.ozerova@list.ru>
2020-04-25 23:37:45 -0700

    Some beta release work

--- js/map.js
+++ js/map.js
@@ -1,55 +1,54 @@
 function drawMap() {
   const urls = {
-    basemap: "https://data.sfgov.org/resource/xfcw-9evu.geojson",
-    //streets: "https://data.sfgov.org/resource/3psu-pn9h.geojson?$limit=20000",
-    //arrests: "https://data.sfgov.org/resource/nwbb-fxkq.json"
-    //basemap: "https://usc.data.socrata.com/resource/9utn-waje.geojson" //LA
+    basemap: "https://data.sfgov.org/resource/6ia5-2f8k.geojson", //SF Find Neighborhoods
+    //  streets: "https://data.sfgov.org/resource/3psu-pn9h.geojson?$limit=15000",
+    //  cases: "https://data.sfgov.org/resource/vw6y-z8j6.geojson"
   };
+
+  // configuration of svg/plot area
+  let config = {
+    'svg': {},
+    'margin': {},
+    'plot': {}
+  };
+
+  let width = 960;
+  let height = 500;
 
   // calculate date range
-  const end = d3.timeDay.floor(d3.timeDay.offset(new Date(), -1));
-  const start = d3.timeDay.floor(d3.timeDay.offset(end, -7));
+  const end = d3.timeDay.floor(d3.timeDay.offset(new Date(), -28));
+  const start = d3.timeDay.floor(d3.timeDay.offset(end, -28));
   const format = d3.timeFormat("%Y-%m-%dT%H:%M:%S");
-  console.log(format(start), format(end));
 
-  // add parameters to arrests url
-  urls.arrests += "?$where=starts_with(resolution, 'Cite or Arrest')";
-  urls.arrests += " AND incident_date between '" + format(start) + "'";
-  urls.arrests += " and '" + format(end) + "'";
-  urls.arrests += " AND point IS NOT NULL";
-
-  // output url before encoding
-  console.log(urls.arrests);
-
-  // encode special characters
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURI
-  urls.arrests = encodeURI(urls.arrests);
-  console.log(urls.arrests);
-
-  const svg = d3.select("body").select("svg#vis");
+  const svg = d3.select("body").select("svg#vis")
+    .attr("style", "outline: thin solid lightgrey;");
+  // setup plot area
+  let plot = svg.select("g#plot")
+  plot.attr('id', 'plot');
+  plot.style("background", "blue");
+  plot.attr('transform', translate(80, 10));
 
   const g = {
-    basemap: svg.select("g#basemap"),
-    streets: svg.select("g#streets"),
-    outline: svg.select("g#outline"),
-    arrests: svg.select("g#arrests"),
-    tooltip: svg.select("g#tooltip"),
-    details: svg.select("g#details")
+    basemap: plot.select("g#basemap"),
+    streets: plot.select("g#streets"),
+    outline: plot.select("g#outline"),
+    movies: plot.select("g#movies"),
+    details: plot.select("g#details"),
+    tooltip: plot.select("g#tooltip"),
+    legend: plot.select("g#legend"),
   };
 
-  // setup tooltip (shows neighborhood name)
-  const tip = g.tooltip.append("text").attr("id", "tooltip");
-  tip.attr("text-anchor", "end");
-  tip.attr("dx", -5);
-  tip.attr("dy", -5);
-  tip.style("visibility", "hidden");
-
-  // add details widget
-  // https://bl.ocks.org/mbostock/1424037
   const details = g.details.append("foreignObject")
     .attr("id", "details")
     .attr("width", 960)
-    .attr("height", 600)
+    .attr("height", 500)
+    .attr("x", 0)
+    .attr("y", 0);
+
+  const tooltip = g.tooltip.append("foreignObject")
+    .attr("id", "tooltip")
+    .attr("width", 960)
+    .attr("height", 500)
     .attr("x", 0)
     .attr("y", 0);
 
@@ -60,33 +59,20 @@ function drawMap() {
 
   details.style("visibility", "hidden");
 
-  // setup projection
-  // https://github.com/d3/d3-geo#geoConicEqualArea
   const projection = d3.geoConicEqualArea();
   projection.parallels([37.692514, 37.840699]);
   projection.rotate([122, 0]);
-
-  // setup path generator (note it is a GEO path, not a normal path)
   const path = d3.geoPath().projection(projection);
 
   d3.json(urls.basemap).then(function(json) {
-    //d3.csv("twin_peaks/WA_Counties.csv").then(function(json) {
-    // makes sure to adjust projection to fit all of our regions
-    projection.fitSize([960, 600], json);
-
-    // draw the land and neighborhood outlines
+    projection.fitSize([900, 500], json);
     drawBasemap(json);
 
-    // now that projection has been set trigger loading the other files
-    // note that the actual order these files are loaded may differ
     d3.json("data_movie/blocks.geojson").then(drawStreets);
-
-    d3.csv("data_movie/film-locations-in-san-francisco_no_dups.csv").then(drawArrests);
-    //  d3.json(urls.arrests).then(drawArrests);
+    d3.csv("data_movie/film-locations-in-san-francisco_no_dups.csv").then(drawMovies);
   });
 
   function drawBasemap(json) {
-    console.log("basemap", json);
 
     const basemap = g.basemap.selectAll("path.land")
       .data(json.features)
@@ -102,40 +88,27 @@ function drawMap() {
       .attr("d", path)
       .attr("class", "neighborhood")
       .each(function(d) {
-        // save selection in data for interactivity
-        // saves search time finding the right outline later
         d.properties.outline = this;
+        d.properties.centroid = path.centroid(d);
       });
+
 
     // add highlight
-    /*basemap.on("mouseover.highlight", function(d) {
+    basemap.on("mouseover.highlight", function(d) {
         d3.select(d.properties.outline).raise();
-        //    console.log(d.properties);
-        d3.select(d.properties.outline).classed("active", true);
+        d3.select(d.properties.outline).classed("activeland", true);
       })
       .on("mouseout.highlight", function(d) {
-        d3.select(d.properties.outline).classed("active", false);
+        d3.select(d.properties.outline).classed("activeland", false);
       });
-
-    // add tooltip
-    basemap.on("mouseover.tooltip", function(d) {
-        tip.text(d.properties.nhood);
-        tip.style("visibility", "visible");
-      })
-      .on("mousemove.tooltip", function(d) {
-        const coords = d3.mouse(g.basemap.node());
-        tip.attr("x", coords[0]);
-        tip.attr("y", coords[1]);
-      })
-      .on("mouseout.tooltip", function(d) {
-        tip.style("visibility", "hidden");
-      });*/
   }
 
   function drawStreets(json) {
-    //console.log("streets", json);
+    console.log("streets", json);
 
-    const streets = json.features;
+    const streets = json.features.filter(function(d) {
+      return d;
+    });
 
     g.streets.selectAll("path.street")
       .data(streets)
@@ -145,8 +118,7 @@ function drawMap() {
       .attr("class", "street");
   }
 
-  function drawArrests(json) {
-    console.log("arrests", json);
+  function drawMovies(json) {
 
     let color = d3.scaleSequential(d3.interpolatePlasma)
       .domain([1915, 2019]);
@@ -159,10 +131,12 @@ function drawMap() {
       const pixels = projection([longitude, latitude]);
       d.x = pixels[0];
       d.y = pixels[1];
-      d.year = d["Release Year"];
+      d.year = parseInt(d["Release Year"]);
     });
 
-    const symbols = g.arrests.selectAll("circle")
+    createSlider(json);
+
+    const symbols = g.movies.selectAll("circle")
       .data(json)
       .enter()
       .append("circle")
@@ -174,6 +148,7 @@ function drawMap() {
 
     symbols.on("mouseover.hover", function(d) {
       d3.select(this).raise();
+      d3.select(this).style("stroke", "black");
       d3.select(this).classed("active-small", true);
 
       let div = d3.select("body").append("div");
@@ -194,12 +169,6 @@ function drawMap() {
       rows.append("th").text(key => key);
       rows.append("td").text(key => dataNew[key]);
       div.style("display", "inline");
-
-      symbols.filter(e => (d.year !== e.year)).transition().style("fill", "#bbbbbb");
-      symbols.filter(function(e) {
-            return d.year == e.year;
-        }).raise();
-
     });
 
     symbols.on("mousemove.hover", function(d) {
@@ -212,17 +181,17 @@ function drawMap() {
 
     symbols.on("mouseout.hover", function(d) {
       d3.select(this).classed("active-small", false);
+      d3.select(this).style("stroke", "");
       d3.selectAll("div#details").remove();
-      symbols.transition().style("fill", d => color(d.type));
+      //symbols.transition().style("fill", d => color(d.type));
     });
-
   }
 
   function translate(x, y) {
     return "translate(" + String(x) + "," + String(y) + ")";
   }
 
-  //function for a second tooltip
+  //function to make tooltip look better
   function createTooltip(row, index) {
 
     let out = {};
@@ -240,15 +209,6 @@ function drawMap() {
         case 'Director':
           out['Director:\xa0'] = row[col];
           break;
-        case 'Actor 1':
-          out['Actor 1:\xa0'] = row[col];
-          break;
-        case 'Actor 2':
-          out['Actor 2:\xa0'] = row[col];
-          break;
-        case 'Actor 3':
-          out['Actor 3:\xa0'] = row[col];
-          break;
 
         default:
           break;
@@ -257,4 +217,59 @@ function drawMap() {
     return out;
   }
 
+  function updateMap(years) {
+
+    let startYear = years[0];
+    let endYear = years[1];
+    const symbols = g.movies.selectAll("circle")
+
+    symbols.filter(d => (d.year > endYear || d.year < startYear)).transition().style("display", "none");
+    symbols.filter(d => (d.year <= endYear && d.year >= startYear)).raise().transition().style("display", "inline");
+  }
+
+  function createSlider(data) {
+
+    let years = new Set(data.map(row => row.year))
+    let minYear = 2020;
+    let maxYear = 0;
+
+    years.forEach(function(d) {
+      if (d < minYear) {
+        minYear = d;
+      }
+      if (d > maxYear) {
+        maxYear = d;
+      }
+    });
+
+    //adding years slider
+    var sliderRange = d3
+      .sliderBottom()
+      .min(minYear)
+      .max(maxYear)
+      .step(1)
+      .width(500)
+      .tickValues(years)
+      .default([1915, 2020])
+      .fill('#2196f3')
+      .on('onchange', value => {
+        d3.select('p#value-simple').text(value.join('-'));
+        updateMap(value);
+      });
+
+    var gTime = d3
+      .select('div#slider-simple')
+      .append('svg')
+      .attr('width', 550)
+      .attr('height', 100)
+      .append('g')
+      .attr('transform', 'translate(30,30)');
+
+    gTime.call(sliderRange);
+
+    d3.select('p#value-simple').text(
+      sliderRange.value()
+      .join('-')
+    );
+  }
 }
